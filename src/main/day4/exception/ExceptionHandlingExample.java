23e26431561b340f80f5717f9e86ee7bbfeec308
techlearn21 <81498369+techlearn21@users.noreply.github.com>
2021-04-04 10:54:35 -0400

    added exception example

--- src/main/day4/exception/ExceptionHandlingExample.java
+++ src/main/day4/exception/ExceptionHandlingExample.java
@@ -2,17 +2,24 @@ package main.day4.exception;
 
 public class ExceptionHandlingExample {
     public static void main(String[] args) {
-        int num1 = 20;
-        int num2 = 0;
+        int num1, num2, num3;
+
+        num1 = 20;
+        num2 = 0;
 
         try {
-            int num3 = num1 / num2;
+            num3 = num1 / num2;
+            System.out.println("No exception occurred. Result of division is: " + num3);
         } catch (ArithmeticException ae) {
+            System.out.println("Numbers cannot be divided by zero");
             System.out.println("ArithmeticException occurred" + ae);
         } catch (Exception e) {
             System.out.println("Exception occurred" + e);
         } finally {
-            System.out.println("In finally");
+            System.out.println("finally block will always be executed");
         }
+
+        num3 = num1 + num2;
+        System.out.println("Result after addition is: " + num3);
     }
 }
