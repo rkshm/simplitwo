9add62e4f53ff43eacd77a522348c4501b038d91
techlearn21 <81498369+techlearn21@users.noreply.github.com>
2021-04-04 11:42:54 -0400

    Added Throws example

---
+++ src/main/day4/exception/ThrowsExample.java
@@ -0,0 +1,31 @@
+package main.day4.exception;
+
+import java.io.IOException;
+
+public class ThrowsExample {
+
+    void m() throws IOException {
+        throw new IOException("device error"); //checked exception
+    }
+
+    void n() throws IOException {
+        m();
+    }
+
+    void p() {
+        try {
+            n();
+        } catch (Exception e) {
+            System.out.println("exception handled");
+            System.out.println(e.getMessage());
+            System.out.println(e.getStackTrace());
+            System.out.println(e.getClass());
+        }
+    }
+
+    public static void main(String[] args) {
+        ThrowsExample obj = new ThrowsExample();
+        obj.p();
+        System.out.println("normal flow ....");
+    }
+}
